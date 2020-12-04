99d500b5a40404317921fd736b50202cd79d4876
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2020-12-04 18:34:18 +1100

    added if else to modify text for drink names that start with a vowel or the

--- The-Cocktail-Shaker/app.js
+++ The-Cocktail-Shaker/app.js
@@ -1,18 +1,23 @@
 console.log("app is running");
 
-// $("#make-drink").on("click", () => {
-//     console.log("make my drink button was clicked");
-// });
-
 // This should return a random cocktail recipe from the The Cocktail DB api and display the name and an image of the cocktail returned:
-$("#make-drink").on("click", () => {
-    console.log("make my drink button was clicked");
-    const drinkData = $.get(
-        `https://www.thecocktaildb.com/api/json/v1/1/random.php`,
-        (data) => {
-            console.log(data.drinks[0].strDrink);
-            $("#drink-name").html(data.drinks[0].strDrink);
-            $("#drink-img").attr("src", data.drinks[0].strDrinkThumb);
-        }
-    ) 
+$("#generate-drink").on("click", () => {
+  console.log("make my drink button was clicked");
+  const drinkData = $.get(
+    `https://www.thecocktaildb.com/api/json/v1/1/random.php`,
+    (data) => {
+      console.log(data.drinks[0].strDrink);
+      const drinkName = data.drinks[0].strDrink;
+      if(drinkName[0] === "A" || drinkName[0] === "E" || drinkName[0] === "I" || drinkName[0] === "O" || drinkName[0] === "U") {
+        $("#drink-name").html(`How about an ${drinkName}?`);
+      } else if (drinkName[0] === "T" && drinkName[1] === "h" && drinkName[2] === "e") {
+        $("#drink-name").html(`How about ${drinkName}?`);
+      } else {
+        $("#drink-name").html(`How about a ${drinkName}?`);
+    }
+      
+      
+      $("#drink-img").attr("src", data.drinks[0].strDrinkThumb);
+    }
+  );
 });
