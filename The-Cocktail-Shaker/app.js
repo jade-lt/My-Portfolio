535df1cfcdcc39f0694a49719933b84553521188
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2020-12-04 17:34:18 +1100

    button created and on click returns drink name and image from api

--- The-Cocktail-Shaker/app.js
+++ The-Cocktail-Shaker/app.js
@@ -1,1 +1,18 @@
 console.log("app is running");
+
+// $("#make-drink").on("click", () => {
+//     console.log("make my drink button was clicked");
+// });
+
+// This should return a random cocktail recipe from the The Cocktail DB api and display the name and an image of the cocktail returned:
+$("#make-drink").on("click", () => {
+    console.log("make my drink button was clicked");
+    const drinkData = $.get(
+        `https://www.thecocktaildb.com/api/json/v1/1/random.php`,
+        (data) => {
+            console.log(data.drinks[0].strDrink);
+            $("#drink-name").html(data.drinks[0].strDrink);
+            $("#drink-img").attr("src", data.drinks[0].strDrinkThumb);
+        }
+    ) 
+});
