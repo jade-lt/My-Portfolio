2f49b48d0b3bc302f9c33d9645aa899d3878eb8f
Jade Lillo-Trynes <jade.lillotrynes@gmail.com>
2020-12-12 18:56:16 +1100

    updated css so that recipe renders more clearly

--- The-Cocktail-Shaker/app.js
+++ The-Cocktail-Shaker/app.js
@@ -1,31 +1,23 @@
+// this hides buttons that should only be visible after the first cocktail idea has been generated
 $(document).ready(function () {
-  // this hides 2 buttons that should only be visible after the first cocktail idea has been generated
   $("#no-thanks-button").hide();
   $("#generate-recipe-button").hide();
+  $("#start-again-button").hide();
   $("#drink-recipe").hide();
 });
 
+// this should return a random cocktail recipe from the The Cocktail DB api and display the name and an image of the cocktail returned
 function newDrinkIdea(buttonInput) {
-  // This should return a random cocktail recipe from the The Cocktail DB api and display the name and an image of the cocktail returned
   $(buttonInput).on("click", () => {
     $.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`, (data) => {
       const drinkData = data.drinks[0];
       const drinkName = drinkData.strDrink;
+      const x = drinkName[0];
 
-      if (
-        // this if else isolates cocktail names that start with a vowel or with "the" and returns a gramatically correct sentence
-        drinkName[0] === "A" ||
-        drinkName[0] === "E" ||
-        drinkName[0] === "I" ||
-        drinkName[0] === "O" ||
-        drinkName[0] === "U"
-      ) {
+// this if else isolates cocktail names that start with a vowel or with "the" and returns a gramatically correct sentence
+      if (x === "A" || x === "E" || x === "I" || x === "O" || x === "U") {
         $("#drink-name").html(`How about an ${drinkName}?`);
-      } else if (
-        drinkName[0] === "T" &&
-        drinkName[1] === "h" &&
-        drinkName[2] === "e"
-      ) {
+      } else if (x === "T" && x === "h" && x === "e") {
         $("#drink-name").html(`How about ${drinkName}?`);
       } else {
         $("#drink-name").html(`How about a ${drinkName}?`);
@@ -40,7 +32,7 @@ function newDrinkIdea(buttonInput) {
 
       const drinkObjectArray = Object.values(drinkData);
       const ingredientAmountArray = drinkObjectArray.slice(36, 51);
-      const drinkIngredientsArray = drinkObjectArray.slice(21, 36);
+      const ingredientsArray = drinkObjectArray.slice(21, 36);
       const drinkInstructions = drinkData.strInstructions;
 
       function returnElements(arr, location) {
@@ -55,22 +47,26 @@ function newDrinkIdea(buttonInput) {
       $("#generate-recipe-button").on("click", () => {
         $("#ingredient-amounts").empty();
         $("#drink-ingredients").empty();
-        console.log(drinkName, ingredientAmountArray, drinkIngredientsArray);
+        console.log(drinkName, ingredientAmountArray, ingredientsArray);
         $("#drink-recipe").show();
-        // $("#ingredient-amounts").html(ingredientAmountArray);
-        // $("#drink-ingredients").html(drinkIngredientsArray);
         returnElements(ingredientAmountArray, "#ingredient-amounts");
-        returnElements(drinkIngredientsArray, "#drink-ingredients");
+        returnElements(ingredientsArray, "#drink-ingredients");
         $("#drink-instructions").html(drinkInstructions);
+        $("#generate-recipe-button").hide();
+        $("#no-thanks-button").hide();
+        $("#start-again-button").show();
       });
 
       $("#no-thanks-button").on("click", () => {
         $("#drink-recipe").hide();
       });
+
+      $("#start-again-button").on("click", () => {
+        window.location.reload();
+      });
+
     });
   });
 }
 
 newDrinkIdea(".generate-drink");
-
-
